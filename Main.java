3a097ad248608c0a64715782e2e5c4c165a4138f
pavithra03042006 <s.pavithrasankar03042006@gmail.com>
2024-12-22 12:37:10 +0530

    created

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void print()
+    {
+        for(int i=1;i<=200;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    public static void main(String a[])
+    {
+        print();
+    }
+}
